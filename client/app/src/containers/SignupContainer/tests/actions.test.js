5b133bd55ca8735137fb41991dabb77bc0a29c9b
Ryan Collins <rccproductions@mac.com>
2016-10-07 22:45:33 -0400

    Feat: add tests for signup actions

--- client/app/src/containers/SignupContainer/tests/actions.test.js
+++ client/app/src/containers/SignupContainer/tests/actions.test.js
@@ -3,12 +3,38 @@ import * as actions from '../actions';
 import * as types from '../constants';
 
 describe('Signup actions', () => {
-  describe('Default Action', () => {
-    it('has a type of DEFAULT_ACTION', () => {
-      const expected = {
-        type: types.SIGNUP_DEFAULT_ACTION,
-      };
-      expect(actions.signupDefaultAction()).toEqual(expected);
-    });
+  it('should handle SIGNUP_SHOW_ERROR', () => {
+    const error = 'An error has occured';
+    const expected = {
+      type: types.SIGNUP_SHOW_ERROR,
+      error,
+    };
+    expect(actions.signupShowError(error)).toEqual(expected);
+  });
+  it('should handle SIGNUP_SHOW_MESSAGE', () => {
+    const message = 'An error has not occured';
+    const expected = {
+      type: types.SIGNUP_SHOW_ERROR,
+      message,
+    };
+    expect(actions.signupShowMessage(message)).toEqual(expected);
+  });
+  it('should handle SIGNUP_CLEAR_ERROR', () => {
+    const expected = {
+      type: types.SIGNUP_CLEAR_ERROR,
+    };
+    expect(actions.signupClearError()).toEqual(expected);
+  });
+  it('should handle SIGNUP_CLEAR_MESSAGE', () => {
+    const expected = {
+      type: types.SIGNUP_CLEAR_MESSAGE,
+    };
+    expect(actions.signupClearMessage()).toEqual(expected);
+  });
+  it('should handle SIGNUP_SET_LOADING', () => {
+    const expected = {
+      type: types.SIGNUP_SET_LOADING,
+    };
+    expect(actions.signupSetLoading()).toEqual(expected);
   });
 });
