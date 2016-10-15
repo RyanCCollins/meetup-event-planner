b885a9148d9e250e3a8bf4acdb5e8cc4b667e715
Ryan Collins <rccproductions@mac.com>
2016-10-15 18:31:47 -0400

    Fix: update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,7 +21,7 @@ Event.create(
   start_date: '10/06/2016 5:00PM',
   end_date: '10/06/2016 7:00PM',
   host: Host.create(name: 'Ryan Collins'),
-  guests: Array.new(7) { Guest.all.sample },
+  guests: 7.times.map{ Guest.all.sample },
   message: 'Join us to celebrate the release of the alumni web application.  We will have an online celebration as well as an in-person meetup at Udacity headquarters.',
   location: '2465 Latham Street, 3rd Floor., Mountain View, CA 94040'
 )
